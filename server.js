93b9819e377954b8f7f8ffb0450b885e8ccec061
afreem66 <afreem66@gmail.com>
2015-10-09 10:39:49 -0400

    added mongouri stuff

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express'),
     PORT = process.env.PORT || 5432,
-    server = express();
+    server = express(),
+    MONGOURI = process.env.MONGOLAB_URI,
+    mongoose = require('mongoose');
 
   server.get('/', function (req, res) {
     res.write("Welcome to my amazing app");
     res.end();
   });
 
+  mongoose.connect(MONGOURI);
   server.listen(PORT, function () {
     console.log("server is up on PORT: ", PORT);
   })
