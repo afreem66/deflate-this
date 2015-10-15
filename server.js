96924270557fe0570c384ed943589a0405b672bf
afreem66 <afreem66@gmail.com>
2015-10-15 19:08:15 -0400

    commented server.js file

--- server.js
+++ server.js
@@ -12,17 +12,21 @@ var express = require('express'),
     session = require('express-session'),
     bcrypt = require('bcryptjs');
 
-///defining path for
+///defining paths for layouts
   server.set('views', './views');
   server.set('view engine', 'ejs');
 
+///setting up sessions
   server.use(session({
     secret: "SOME PASSPHRASE TO ENCRYPT",
     resave : true,
     saveUninitialized: true
   }));
 
-///setting up packages
+///telling the app to use express and where to find static files, methodOverride
+///and body parser to show us nice bodies when asked ;) adn morgan to log errors
+///and express-ejs-layouts to make our lives easier when creating the way the
+///app is formatted
 server.use(express.static('./public'));
 
 server.use(methodOverride('_method'));
@@ -35,15 +39,20 @@ server.use(morgan('dev'));
 
 server.use(expressLayouts);
 
+///this allows me to access the currentUser variable whereever I am since it is
+///stored locally
 server.use(function (req, res, next) {
   res.locals.user = req.session.currentUser;
   next();
 })
-///server pathways
+
+///requiring users pathway using a controller. All routes from this pathway will
+///begin with /users/
 var usersController = require('./controllers/users.js');
 server.use('/users', usersController);
 
-///server pathways
+///requiring posts pathway using a controller. All routes from this pathway will
+///begin with /posts/
 var postsController = require('./controllers/posts.js');
 server.use('/posts', postsController);
 
@@ -53,16 +62,16 @@ server.get('/welcome', function (req, res) {
   });
 });
 
-//catch all pathway brings you back to login or create user page
+///catch all pathway brings you back to login or create user page
 server.use('/', function(req, res) {
   res.render('loginWall');
 });
 
 
-//connecting mongoose
+//connecting mongoose locally and on heroku
 mongoose.connect(MONGOURI + "/" + dbname);
 
-///setting server to listen
+///setting server to listen locally and on heroku
   server.listen(PORT, function () {
     console.log("I LOVE WALTAH");
   })
