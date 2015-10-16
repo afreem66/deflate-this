96cfbe49b8c7b28b920e5c5f358322d41ff9f0b1
afreem66 <afreem66@gmail.com>
2015-10-15 20:08:00 -0400

    finished commenting css

--- models/postModel.js
+++ models/postModel.js
@@ -1,5 +1,12 @@
+///requiring mongoose and defining a Schema
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+
+///creating a new schema for posts. including a title, author, their viewpoint,
+///upvotes (defaulted to 0), the post content, the date created, and an array of
+///of comments which holds objects that have an author and content and adds it
+/// to the posts collection
 
 var postSchema = new Schema ({
   title: {type: String},
@@ -11,6 +18,10 @@ var postSchema = new Schema ({
   comments: [ {author: String, content: String} ]
 }, {collection: 'post', strict : false});
 
+///this is defining the model or collection with the schema
+
 var Post = mongoose.model('Post', postSchema)
+
+///exporting the model to server.js
 
 module.exports = Post;
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,10 @@
+///requiring mongoose and defining a Schema
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+
+///creating a new schema for posts. including a username, encrypted password,
+/// their name, and viewpoint. it also adds it to the collection user
 
 var userSchema = new Schema ({
   username: {type : String},
@@ -9,6 +14,10 @@ var userSchema = new Schema ({
 
 }, {collection: 'user', strict: false});
 
+///this is defining the model  with the schema
+
 var User = mongoose.model('User', userSchema)
+
+///exporting to server.js
 
 module.exports = User;
